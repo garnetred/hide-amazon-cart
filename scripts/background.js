8847a20e7d4892a0e557b3c90fb1db08a82344c5
December G <dgarnetsmith@gmail.com>
2023-10-16 23:13:05 -0500

    Add new domains for testing and increment version number

--- scripts/background.js
+++ scripts/background.js
@@ -1,8 +1,21 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   const tabUrl = tab.url ?? tab.pendingUrl;
   if (
-    changeInfo.status === 'complete' &&
-    tabUrl &&
+    (changeInfo.status === 'complete' &&
+      tabUrl &&
+      tabUrl.includes('amazon.de')) || //validated
+    tabUrl.includes('amazon.es') || //validated
+    tabUrl.includes('amazon.fr') ||
+    tabUrl.includes('amazon.in') || // validated
+    tabUrl.includes('amazon.it') ||
+    tabUrl.includes('amazon.co.uk') || // validated
+    tabUrl.includes('amazon.ca') || // validated
+    tabUrl.includes('amazon.cn') ||
+    tabUrl.includes('amazon.co.jp') ||
+    tabUrl.includes('amazon.nl') ||
+    tabUrl.includes('amazon.sa') ||
+    tabUrl.includes('amazon.com.au') ||
+    tabUrl.includes('amazon.com.mx') ||
     tabUrl.includes('amazon.com')
   ) {
     chrome.scripting.insertCSS({
